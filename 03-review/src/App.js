5b99d1b40163467318a7ad7fba5582984015d4ea
alxayeed <alxayeed@gmail.com>
2021-02-07 19:48:32 +0600

    review app created and project files cleaned up

---
+++ 03-review/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <main >
+        <section className="container">
+          <div className="title">
+            <h2>Our reviews</h2>
+            <div className="underline"></div>
+          </div>
+        </section>
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
