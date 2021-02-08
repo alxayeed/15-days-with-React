8dd37bf37db6364e183c04578c055ba502cbfe8e
alxayeed <alxayeed@gmail.com>
2021-02-08 11:11:42 +0600

    project 04 created and cleaned up for starting development

---
+++ 04-accordion/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <header className="">
+        <h1>Hola</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
