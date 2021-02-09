68b2d77598e6aa79e085efe4f1964646cbe9ec6f
alxayeed <alxayeed@gmail.com>
2021-02-09 22:33:38 +0600

    menu component implemented

--- 05-menu/src/App.js
+++ 05-menu/src/App.js
@@ -1,14 +1,22 @@
+import React, { useState } from 'react'
 import Catagory from "./Catagory";
 import Menu from "./Menu";
+import items from './Data'
 
 function App() {
+  const [menuItems, setMenuItems] = useState(items)
+  const [catagories, setCatagories] = useState([])
   return (
-    <div className="App">
-      <h2>App Component</h2>
-      <Menu />
-      <Catagory />
-
-    </div>
+    <main>
+      <section className='menu section'>
+        <div className='title'>
+          <h2>our menu</h2>
+          <div className='underline'></div>
+        </div>
+        <Menu items={menuItems} />
+        <Catagory />
+      </section>
+    </main>
   );
 }
 
