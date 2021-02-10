e200adf73ade76496612c8cb4776f58175f1cf04
alxayeed <alxayeed@gmail.com>
2021-02-10 19:51:39 +0600

    items are now shown all one top of another

--- 07-slider/src/App.js
+++ 07-slider/src/App.js
@@ -1,21 +1,41 @@
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+import React, { useState, useEffect } from 'react'
+import data from './Data'
+import { BiChevronLeftCircle, BiChevronRightCircle } from "react-icons/bi";
 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+function App() {
+  const [people, setPeople] = useState(data)
+  const [index, setIndex] = useState(0)
+
+
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, personIndex) => {
+          const { id, image, name, title, quote } = person;
+
+          return <article key={id}>
+            <img src={image} alt={name} className="person-img" />
+            <h4>{name}</h4>
+            <p className="title">{title}</p>
+            <p className="text">{quote}</p>
+
+          </article>
+        })}
+        <button className="prev">
+          <BiChevronLeftCircle />
+        </button>
+        <button className="next">
+          <BiChevronRightCircle />
+        </button>
+      </div>
+    </section>
   );
 }
 
