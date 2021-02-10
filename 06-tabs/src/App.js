23135ed53edee77eebdcf60a19f87ad1f2eebfb7
alxayeed <alxayeed@gmail.com>
2021-02-10 15:31:41 +0600

    api fetching data.loading icon implemented

--- 06-tabs/src/App.js
+++ 06-tabs/src/App.js
@@ -1,11 +1,42 @@
+import React, { useState, useEffect } from 'react'
+import { GiMoebiusTriangle } from 'react-icons/gi'
 import logo from './logo.svg';
+const url = 'https://course-api.com/react-tabs-project'
+
 
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0)
+
+  const fetchJobs = async () => {
+    const response = await fetch(url)
+    const newJobs = response.json()
+    setJobs(newJobs);
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    fetchJobs();
+  }, [])
+
+  console.log(jobs, loading)
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <section className="section loading">
+          <img src={logo} className="" alt="logo" />
+        </section>
+      </section>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="">
+      <header className="">
+        <h1>Jobs</h1>
 
       </header>
     </div>
