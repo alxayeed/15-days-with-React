62b8507f9abaaf15fa5e367076e4ae31fdb8d388
alxayeed <alxayeed@gmail.com>
2021-02-12 14:26:33 +0600

    basic component structured

--- 08-lorem/src/App.js
+++ 08-lorem/src/App.js
@@ -1,10 +1,32 @@
-
+import React, { useState } from 'react'
+import data from './Data'
 
 function App() {
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(e)
+  }
+
   return (
-    <div className="App">
-      <h1>Lorem ipsum </h1>
-    </div>
+    <section className="section-center">
+      <h2>Make some lorem</h2>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">
+          paragraphs:
+        </label>
+        <input type="number" name="amount" id="amount"
+          value={count} onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">Let's go!</button>
+      </form>
+      <article>
+        <p>Rutters Plate Fleet boom chandler Brethren of the Coast handsomely lookout marooned brigantine knave. Buccaneer gangway jack rum loot spyglass line Jack Tar fore gaff. Gaff topmast scuttle ballast swab draught measured fer yer chains dance the hempen jig Chain Shot yardarm.</p>
+        <p>Rutters Plate Fleet boom chandler Brethren of the Coast handsomely lookout marooned brigantine knave. Buccaneer gangway jack rum loot spyglass line Jack Tar fore gaff. Gaff topmast scuttle ballast swab draught measured fer yer chains dance the hempen jig Chain Shot yardarm.</p>
+      </article>
+    </section>
   );
 }
 
